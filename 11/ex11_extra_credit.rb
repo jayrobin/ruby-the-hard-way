6edf1d78f1ce1befe033ae777e2c735b4d67e043
casinojack <casinojack@googlemail.com>
2013-04-08 20:44:36 +0100

    Completed exercises 6 to 13

---
+++ 11/ex11_extra_credit.rb
@@ -0,0 +1,10 @@
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+print "Where were you born? "
+birthplace = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} tall, #{weight} heavy and you were born in #{birthplace}."
